72616e97b69ea96e7b8a924017df5d78268badce
e22111527 <oumaima.ayad@etudiant.univ-brest.fr>
2024-11-12 17:46:16 +0100

    modif

--- TestProjet/src/location/Artiste.java
+++ TestProjet/src/location/Artiste.java
@@ -3,5 +3,45 @@ package location;
 // A COMPLETER
 
 public class Artiste {
+	 private String nom;
+	 private String prenom;
+	 private String role; 
+	 
+	 public Artiste(String nom, String prenom, String role) {
+	        this.nom = nom;
+	        this.prenom = prenom;
+	        this.role = role;
+	    }
 
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	     
+	 
+	@Override
+    public String toString() {
+        return prenom + " " + nom + " (" + role + ")";
+	}
+	 
+	 
 }
--- TestProjet/src/location/Film.java
+++ TestProjet/src/location/Film.java
@@ -3,5 +3,22 @@ package location;
 // A COMPLETER
 
 public class Film {
+	private String titre;
+	private int annee;
+	private Artiste realisateur;
+	private Set<Artiste> acteurs;
+	private Set<Genre> genres;
+	private int ageMinimum;
+    
+	 public Film(String titre, int annee, Artiste realisateur, int ageMinimum) {
+	        this.titre = titre;
+	        this.annee = annee;
+	        this.realisateur = realisateur;
+	        this.ageMinimum = ageMinimum;
+	        this.acteurs = new ArrayList<>();
+	        this.genres = new ArrayList<>();
+	        }
+    
 
 }
+ 
