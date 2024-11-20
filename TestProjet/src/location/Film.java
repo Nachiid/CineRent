7839dd2a389cfe7f3eff527c5a3f450e2b610802
e22111527 <oumaima.ayad@etudiant.univ-brest.fr>
2024-11-20 13:44:30 +0100

    modif

--- TestProjet/src/location/Film.java
+++ TestProjet/src/location/Film.java
@@ -9,16 +9,17 @@ public class Film {
 	private Set<Artiste> acteurs;
 	private Set<Genre> genres;
 	private int ageMinimum;
+	
+	
+	public Film(String titre, int annee, Artiste realisateur, Set<Artiste> acteurs, Set<Genre> genres, int ageMinimum) {
+		super();
+		this.titre = titre;
+		this.annee = annee;
+		this.realisateur = realisateur;
+		this.acteurs = acteurs;
+		this.genres = genres;
+		this.ageMinimum = ageMinimum;
+	}
     
-	 public Film(String titre, int annee, Artiste realisateur, int ageMinimum) {
-	        this.titre = titre;
-	        this.annee = annee;
-	        this.realisateur = realisateur;
-	        this.ageMinimum = ageMinimum;
-	        this.acteurs = new ArrayList<>();
-	        this.genres = new ArrayList<>();
-	        }
-    
-
+	
 }
- 
